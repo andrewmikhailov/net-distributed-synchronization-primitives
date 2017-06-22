a429f07caecae49816c4f0bd6bf0c40b34695be5
Andrew Mikhailov <developer@vitche.com>
2017-06-22 16:42:59 +0100

    1. Redis distributed mutex compatible with the 'node-mutex' library.

---
+++ DistributedSynchronizationPrimitives/Mutex/RedisMutex.cs
@@ -0,0 +1,33 @@
+﻿namespace DistributedSynchronizationPrimitives.Mutex
+{
+    using StackExchange.Redis;
+
+    public class RedisMutex
+    {
+        ConnectionMultiplexer connection;
+
+        public RedisMutex(string connectionString)
+        {
+            this.connection = ConnectionMultiplexer.Connect(connectionString);
+        }
+
+        public bool Acquire(string key, int expire)
+        {
+            RedisValue strExpire = expire.ToString();
+            var script = "local locked = redis.call('SETNX', KEYS[1], ARGV[1]);" +
+                "if locked == 1 then redis.call('PEXPIRE', KEYS[1], ARGV[2]) end;" +
+                "return locked";
+            IDatabase database = connection.GetDatabase();
+            RedisResult result = database.ScriptEvaluate(script, new RedisKey[] { key }, new RedisValue[] { key, strExpire });
+            return (bool)result;
+        }
+
+        public void Release(string key)
+        {
+            var script = "local id = redis.call('GET', KEYS[1]);" +
+                "if id == ARGV[1] then redis.call('DEL', KEYS[1]) end";
+            IDatabase database = connection.GetDatabase();
+            RedisResult result = database.ScriptEvaluate(script, new RedisKey[] { key }, new RedisValue[] { key });
+        }
+    }
+}
---
+++ DistributedSynchronizationPrimitives/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("DistributedSynchronizationPrimitives")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DistributedSynchronizationPrimitives")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5241f8d8-4342-4739-a54a-d4af4ae00fd9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
